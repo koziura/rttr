f183d6c6401ddfe87eb3f2650047fbb928fd6caf
serge koziura <sergey.kozyura@mytona.com>
2025-07-22 11:25:03 +0200

    C++20 compatibility fixes and modern CMake support
    - Fixed std::vector<argument> default parameter issue in type.h:682
    - Split variant create() method into two overloads to avoid incomplete type problems
    - Updated CMake requirement from 3.0 to 3.25
    - Removed CMake deprecation warnings

--- src/rttr/type.cpp
+++ src/rttr/type.cpp
@@ -192,6 +192,11 @@ variant type::get_metadata(const variant& key) const
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
+variant type::create() const
+{
+    return create(vector<argument>());
+}
+
 variant type::create(vector<argument> args) const
 {
     auto& ctors = m_type_data->m_class_data.m_ctors;
